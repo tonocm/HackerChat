13f695a778d049dc71bab7ce4b590f718cf60a03
tonocm <tonocm94@hotmail.com>
2014-11-01 04:56:11 -0400

    Port Fixed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http')
-var port = process.env.PORT || 1800;
+var port = process.env.PORT || 80;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Welcome to HackerChat\nMade by Antonio and Edward in YHack 2014\n');
