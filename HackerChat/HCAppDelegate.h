1ba420971845279ca9699429a0c595f0da466568
tonocm <tonocm94@hotmail.com>
2014-11-01 03:03:39 -0400

    Migrating Repository

---
+++ HackerChat/HCAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  HCAppDelegate.h
+//  HackerChat
+//
+//  Created by Edward Aryee on 10/31/14.
+//  Copyright (c) 2014 A&E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface HCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackerChat/HCHomeVC.h
@@ -0,0 +1,14 @@
+//
+//  HCHomeVC.h
+//  HackerChat
+//
+//  Created by Edward Aryee on 10/31/14.
+//  Copyright (c) 2014 A&E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface HCHomeVC : UIViewController
+
+@end
