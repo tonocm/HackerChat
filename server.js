d3c1acce60018206038e590eaef5f69d61a3f8b7
tonocm <tonocm94@hotmail.com>
2014-11-15 06:55:01 -0500

    Switching Working Directories

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-var port = process.env.PORT || 80;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to HackerChat\nMade by Antonio and Edward in YHack 2014\n');
-}).listen(port);
