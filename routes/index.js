e583038fc4cbbc486bef34803f0b9a4084b122f6
Eddie Elizondo <eduardo.elizondorueda@gmail.com>
2014-11-15 07:10:40 -0500

    fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  if(!global.user)
+  if(global.user)
   	res.render('dashboard', {pageData: {title: 'Dashboard', userName: global.user.displayName}})
   else 
   	res.render('index', { pageData: {title: 'Express'}});
