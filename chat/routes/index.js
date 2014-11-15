eaf72ced8deb2ebaa19e4390adad8a2500af2343
Eddie Elizondo <eduardo.elizondorueda@gmail.com>
2014-11-15 04:41:31 -0500

    login

--- chat/routes/index.js
+++ chat/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { pageData: {title: 'HackerChat'}});
 });
+
+router.post('/login',
+
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
 
 module.exports = router;
