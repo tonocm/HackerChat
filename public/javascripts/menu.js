baad5aa6a57aa711cd52d7b3b54c6fba1edb0d58
Nicole Giggey <nmgiggey@gmail.com>
2014-11-16 09:26:51 -0500

    i love css

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,18 +1,34 @@
 //SIDE MENU SCRIPTS
  $(document).ready(function () {
 
- 	$('.menu-open-icon').toggleClass('fixed');
+    //$('.menu-open-icon').toggleClass('fixed');
 
     $('.menu-close-icon').click(function (e) {
         e.preventDefault();
         $('#side-menu').animate({ left: '-250px', top: '-1000px' });
-        $('.menu-open-icon').animate({ left: '22px', top: '22px' });
+        $('.menu-open-icon').toggleClass("menu-toggle");
        
     });
     $('.menu-open-icon').click(function (e) {
         e.preventDefault();
-        $('.menu-open-icon').animate({ left: '-100px', top: '-100px' });
-        $('#side-menu').animate({ left: '0px', top: '0px' });
-       
+        $('#side-menu').animate({ left: '0px', top: '0px'});
+        $('.menu-open-icon').toggleClass("menu-toggle");     
     });
+
+    function detectmob() { 
+        if(navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+        ){
+           $(".about-copy").addClass("about-copy-toggle");
+           return true;
+        }
+        else {
+           return false;
+        }
+    }
  });
